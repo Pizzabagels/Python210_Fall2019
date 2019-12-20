76a33fefb1563dfb806e301f62c92da139d22f4c
Tommy <taguilu01@gmail.com>
2019-12-20 07:27:41 -0800

    Uploading mailroom and circle homeworks

--- students/Tommy Aguilu/lesson07/html_render.py
+++ students/Tommy Aguilu/lesson07/html_render.py
@@ -6,36 +6,39 @@ class Element:
     Tag = 'html'
 
     def __init__(self, content=None, **kwargs):
-        self.Content = []  # Content will, ultimately, be a list of elements
+        self.Content = []
         self.Content.append(content);
         self.attributes = {**kwargs}
 
     def append(self, new_content):
         self.Content.append(new_content);
 
-    def render(self, out_file=None):  # OK, it should generate string and write to file
+    def render(self, out_file=None):
         Content = ''
-        attributes = []
         for i in self.Content:
             if i is None:
                 pass
             else:
                 try:
-                    Content += i.render()  # more or less. But, what it would return??? If outfile is not specified, it will NOT
-                except AttributeError:  # This is VERY UGLY, to detect base case
+                    Content += i.render()
+                except AttributeError:
                     Content += i
-                # write to file, right, just return
-                # We also need specify base case for recursion!
+        if self.attributes is None:
+            pass
+        elif self.attributes is not None:
+            #get this working
+            values = ""
+            for k,v in self.attributes.items():
+                values += (str(k)+"="+ str(v))
         String = '<' + self.Tag + '>\n' + Content + '\n' + \
                  '</' + self.Tag + '>\n'
-
         if out_file is None:
             pass
         else:
             out_file.write(String)
             #writes outfile of formatted String value
 
-        return String  # we need to return. When we call recursively, we will have file argument just for first
+        return String
 
 
 class Body(Element):
@@ -64,7 +67,6 @@ class OnelineTag(Element):
                     Content += i
                 # write to file, right, just return
                 # We also need specify base case for recursion!
-
         String = '<' + self.Tag + '> ' + Content + ' </' + self.Tag + '>'
 
         if out_file is None:
@@ -83,7 +85,6 @@ body = Body()
 head = Head()
 title = Title()
 body.append(P("a very small paragraph"))
-body.append(P("another small paragraph", id="TheList", style="line-height:200%"))
 head.append(Title("PythonClass - Session 6 example"))
 page.append(head)
 page.append(body)
